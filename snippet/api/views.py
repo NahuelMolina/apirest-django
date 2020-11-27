c8c904f5a8d695f2f509d6684209433992bba596
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-11-27 17:50:35 -0300

    Update views.py

--- snippet/api/views.py
+++ snippet/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from django.contrib.auth.models import User
 
 from snippet.models import mymodel
@@ -14,7 +14,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
+    # we can use the next method instead of queryset
+    # def get_queryset(self):
+        # return self.request.user.accounts.all()
+
 
 class SomethingViewSet(viewsets.ModelViewSet):
 	queryset = mymodel.objects.all()
 	serializer_class = mymodel_serializer
+
