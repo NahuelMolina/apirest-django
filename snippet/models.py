9b9ed1dd15ca4a11c7ebd267e6b779a400e3ccf3
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-11-27 18:11:37 -0300

    Update models.py

--- snippet/models.py
+++ snippet/models.py
@@ -3,4 +3,8 @@ from django.db import models
 class mymodel(models.Model):
 	name = models.CharField(max_length = 10)
 	last_name = models.CharField(max_length=10)
-
+       
+class Customer(models.Model):
+        name = models.CharField(max_length = 10)
+        nation = models.CharField(max_length = 10)
+        
