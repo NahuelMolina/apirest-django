e62af8feeb68a494281e3d7286f7e7af5cb2ed7c
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-11-27 18:04:11 -0300

    Update views.py

--- snippet/api/views.py
+++ snippet/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 
 from snippet.models import mymodel
@@ -8,6 +9,18 @@ from .serializers import UserSerializer, mymodel_serializer
 
 # Here is where the models serializers and the objects created on terms of this models, 
 # are connected
+
+class UserViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
 class UserViewSet(viewsets.ModelViewSet):
     
