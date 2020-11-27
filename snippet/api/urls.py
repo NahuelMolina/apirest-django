3dec89b28e194366422797bcca5548043b9e2891
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-11-27 18:35:44 -0300

    Update urls.py

--- snippet/api/urls.py
+++ snippet/api/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import url, include
 from rest_framework import routers
 
 from .views import UserViewSet, SomethingViewSet
+from .views import CustomerViewSet
 
 router = routers.DefaultRouter()
+
 router.register(r'thats_users_data', UserViewSet)
 router.register(r'data_prove', SomethingViewSet)
+router.register(r'customer_db_prove',CustomerViewSet)
 
 urlpatterns = [
 	path('', include(router.urls))
